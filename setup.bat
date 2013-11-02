a999229ac10cf2295873b2fa31c472285dacaec2
Ivan Moore <ivan@teamoptimization.com>
2013-11-02 20:17:28 +0000

    simple git config - customised for me

---
+++ setup.bat
@@ -0,0 +1,8 @@
+git config --global user.name "Ivan Moore"
+git config --global user.email "ivan@teamoptimization.com"
+git config --global alias.rb rebase
+git config --global alias.st status
+git config --global alias.ci commit
+git config --global alias.co checkout
+git config --global alias.lg log -p
+git config --global push.default simple
