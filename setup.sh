2e29cbb01b3b02b6eb3c3682a5b3d28bd6cffec6
Ivan Moore <ivan@teamoptimization.com>
2015-03-16 21:54:56 +0000

    updating intelliJ

--- setup.sh
+++ setup.sh
@@ -5,8 +5,8 @@ sudo apt-get update
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
 sudo apt-get install oracle-java8-installer
 sudo apt-get install oracle-java8-set-default
-wget http://download.jetbrains.com/idea/ideaIC-13.1.3.tar.gz
+wget https://download.jetbrains.com/idea/ideaIC-14.0.3.tar.gz
 mkdir ~/install
-tar -xvf ideaIC-13.1.3.tar.gz -C ~/install
-ln -s install/idea-IC-135.909/bin/idea.sh idea.sh
+tar -xvf ideaIC-14.0.3.tar.gz -C ~/install
+ln -s ~/install/idea-IC-139.1117.1/bin/idea.sh ~/idea.sh
 
