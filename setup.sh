b1619f160de7d0afa929caf751855807430c84d1
Ivan Moore <ivan@teamoptimization.com>
2015-03-16 22:08:16 +0000

    must have curl

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-sudo apt-get install git
-sudo apt-get install kdiff3
+sudo apt-get update
+sudo apt-get install git kdiff3 curl
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
