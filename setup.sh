b63eef2e389397f8c267dd935a152e652136c8a8
Ivan Moore <ivan@teamoptimization.com>
2015-04-03 15:34:11 +0100

    don't ask

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 sudo apt-get update
-sudo apt-get install git kdiff3 curl
+sudo apt-get install -y git kdiff3 curl
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-sudo apt-get install oracle-java8-installer
-sudo apt-get install oracle-java8-set-default
+sudo apt-get install -y oracle-java8-installer
+sudo apt-get install -y oracle-java8-set-default
 wget https://download.jetbrains.com/idea/ideaIC-14.0.3.tar.gz
 mkdir ~/install
 tar -xvf ideaIC-14.0.3.tar.gz -C ~/install
