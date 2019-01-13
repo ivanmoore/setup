8b26b3a40ed2f3479f669fb915f6a78f8782e6a5
Ivan Moore <ivan@teamoptimization.com>
2019-01-13 13:34:21 +0000

    new version of intellij

--- setup.sh
+++ setup.sh
@@ -6,8 +6,8 @@ sudo apt-get update
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
 sudo apt-get install -y oracle-java8-installer
 sudo apt-get install -y oracle-java8-set-default
-wget https://download.jetbrains.com/idea/ideaIC-15.0.tar.gz
+wget https://download-cf.jetbrains.com/idea/ideaIC-2018.3.3.tar.gz
 mkdir ~/install
-tar -xvf ideaIC-15.0.tar.gz -C ~/install
-ln -s ~/install/idea-IC-143.381.42/bin/idea.sh ~/idea.sh
+tar -xvf ideaIC-2018.3.3.tar.gz -C ~/install
+ln -s ~/install/idea-IC-183.5153.38/bin/idea.sh ~/idea.sh
 
