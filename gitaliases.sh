bc8a48ef73e740289752860eeaf89700e848c559
Ivan Moore <ivan@teamoptimization.com>
2022-03-17 15:56:33 +0000

    added missing git alias

--- gitaliases.sh
+++ gitaliases.sh
@@ -3,6 +3,7 @@
 git config --global user.name "Ivan Moore"
 git config --global user.email "ivan@teamoptimization.com"
 git config --global alias.rb rebase
+git config --global alias.br branch
 git config --global alias.st status
 git config --global alias.ci commit
 git config --global alias.co checkout
